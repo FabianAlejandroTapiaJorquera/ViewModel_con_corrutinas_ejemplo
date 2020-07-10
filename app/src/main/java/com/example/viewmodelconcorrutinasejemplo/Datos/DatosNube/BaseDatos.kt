0294266a7e5cebe9eba03de2af5eefbc95d58c9c
FabianAlejandroTapiaJorquera <fabian_alejandro_tapia@hotmail.com>
2020-07-10 16:58:24 -0400

    Obtención de datos desde Firestore

--- app/src/main/java/com/example/viewmodelconcorrutinasejemplo/Datos/DatosNube/BaseDatos.kt
+++ app/src/main/java/com/example/viewmodelconcorrutinasejemplo/Datos/DatosNube/BaseDatos.kt
@@ -1,7 +1,10 @@
 package com.example.viewmodelconcorrutinasejemplo.Datos.DatosNube
 
+import androidx.lifecycle.MutableLiveData
 import com.example.viewmodelconcorrutinasejemplo.Datos.Usuario
 import com.example.viewmodelconcorrutinasejemplo.ObjetosSealed.Resultado
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
 
 /**
  * Aquí se realizan todas las operaciones de Firebase para obtener el usuario actual
@@ -10,6 +13,9 @@ import com.example.viewmodelconcorrutinasejemplo.ObjetosSealed.Resultado
 class BaseDatos: IBaseDatos {
 
     override suspend fun obtenerUsuario(): Resultado<Usuario> {
-        return Resultado.Exito(Usuario("Fabian"))
+        val nombreUsuario = FirebaseFirestore.getInstance().collection("usuarios")
+            .document("19230622-1").get().await()
+        val usuario = Usuario(nombreUsuario.get("nombre").toString())
+        return Resultado.Exito(usuario)
     }
 }
